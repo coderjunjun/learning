2844099e514cd06702477d95fc6d4e9a6a1239f2
陈君 <cj234187@alibaba-inc.com>
2020-04-10 19:29:59 +0800

    增加算法类模板

---
+++ .idea/fileTemplates/Algorithm Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+ * <a href=""></a>
+ * <pre>
+ * </pre>
+ *
+ * @author coderjunjun@gmail.com
+ * @date ${DATE}
+ */
+public class ${NAME} {
+}
